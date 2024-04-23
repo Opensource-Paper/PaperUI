3cdf294132a5b65e345fc76e91bc08308a30cb31
Brick_Wall <118539051+GreenestGoat@users.noreply.github.com>
2024-04-23 21:13:17 +0100

    Update import.js

--- import.js
+++ import.js
@@ -1,32 +1,32 @@
 window.addEventListener('load', loadLibrary);
 
-// import librarys
+// import libraries
 
 function loadLibrary() {
   // Add CSS links
   var cssLinks = [
-    '/components/button/button.css',
-    '/components/switch/switch.css',
+    'components/button/button.css',
+    'components/switch/switch.css',
     // Add more CSS links here...
   ];
 
   cssLinks.forEach(function(link) {
     var cssTag = document.createElement('link');
     cssTag.rel = 'stylesheet';
-    cssTag.href = link.startsWith('/') ? link : '/' + link;
+    cssTag.href = 'https://cdn.jsdelivr.net/gh/GreenestGoat/Onyx-UI/' + link;
     document.head.appendChild(cssTag);
   });
 
   // Add JS scripts
   var jsScripts = [
-    '/components/button/button.js',
-    '/components/switch/switch.js',
+    'components/button/button.js',
+    'components/switch/switch.js',
     // Add more JS scripts here...
   ];
 
   jsScripts.forEach(function(script) {
     var jsTag = document.createElement('script');
-    jsTag.src = script.startsWith('/') ? script : '/' + script;
+    jsTag.src = 'https://cdn.jsdelivr.net/gh/GreenestGoat/Onyx-UI/' + script;
     document.head.appendChild(jsTag);
   });
 }
