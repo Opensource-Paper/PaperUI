494271f3eb9aaf6301c487001b11595ac91127a9
Brick_Wall <118539051+GreenestGoat@users.noreply.github.com>
2024-04-23 20:32:05 +0100

    Create library.js

---
+++ library.js
@@ -0,0 +1,26 @@
+function loadLibrary() {
+  // Add CSS links
+  var cssLinks = [
+    'https://path/to/cdn/button.css',
+    'https://path/to/cdn/switch.css',
+  ];
+
+  cssLinks.forEach(function(link) {
+    var cssTag = document.createElement('link');
+    cssTag.rel = 'stylesheet';
+    cssTag.href = link;
+    document.head.appendChild(cssTag);
+  });
+
+  // Add JS scripts
+  var jsScripts = [
+    'https://path/to/cdn/button.js',
+    'https://path/to/cdn/switch.js',
+  ];
+
+  jsScripts.forEach(function(script) {
+    var jsTag = document.createElement('script');
+    jsTag.src = script;
+    document.head.appendChild(jsTag);
+  });
+}
