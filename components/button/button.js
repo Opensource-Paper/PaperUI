57b2598a4f2b2914d9da24c0c34c4ff401c46c0c
Brick_Wall <118539051+GreenestGoat@users.noreply.github.com>
2024-04-24 00:16:55 +0100

    Update button.js

--- components/button/button.js
+++ components/button/button.js
@@ -1,1 +1,17 @@
+// Find all elements with the specified classes
+const buttons = document.querySelectorAll('media-button-filled, media-button-text, media-button-outlined');
 
+// Add click event listener to buttons
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    // Retrieve the specified link from the button's attribute
+    const link = button.getAttribute('link');
+
+    // Redirect to the specified link or perform default action
+    if (link) {
+      window.location.href = link;
+    } else {
+      console.log('Button clicked');
+    }
+  });
+});
