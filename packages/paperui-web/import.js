4eefd4445b39e5334af772c92af5212919fe3dc7
GreenestGoat <fionnanloughlin@gmail.com>
2024-06-06 15:44:16 +0100

    update npm

--- packages/paperui-web/import.js
+++ packages/paperui-web/import.js
@@ -1,1 +1,25 @@
 // import module with CDN
+
+// import components
+
+import './components/button/button.js';
+import './components/checkbox/checkbox.js';
+import './components/fab/fab.js';
+import './components/search/search.js';
+import './components/switch/switch.js';
+
+// import theming
+
+import './components/theme/theme.js';
+
+// export components
+
+export * from './components/button/button.js';
+export * from './components/checkbox/checkbox.js';
+export * from './components/fab/fab.js';
+export * from './components/search/search.js';
+export * from './components/switch/switch.js';
+
+// import theming
+
+export * from './components/theme/theme.js';
